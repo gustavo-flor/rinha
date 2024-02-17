08f7e60c6f5c8d79a610d2295c9888987c225a46
gustavo-flor <gustavo.flor@picpay.com>
2024-02-17 14:00:40 -0300

    :wrench: Adding performance improvements

--- src/main/java/com/github/gustavoflor/rinha/Application.java
+++ src/main/java/com/github/gustavoflor/rinha/Application.java
@@ -2,8 +2,10 @@ package com.github.gustavoflor.rinha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.github.gustavoflor.rinha")
+@EnableJpaRepositories(basePackages = "com.github.gustavoflor.rinha.core.repository")
 public class Application {
 
 	public static void main(String[] args) {
